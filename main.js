ec6c26a5f41de1c83f8522b277f9a7d914b46b8a
Agnes-Jocelyn <62274975+Agnes-Jocelyn@users.noreply.github.com>
2021-11-14 21:32:44 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$('body')
+  .on('click', 'div.fourteen button.btn-search', function(event) {
+    event.preventDefault();
+    var $input = $('div.fourteen input');
+    $input.focus();
+    if ($input.val().length() > 0) {
+      // submit form
+    }
+  })
+;
